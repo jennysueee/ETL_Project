d3a1b803121cb6b0fc4544cad3f060dfd9890376
jennysueee <56518268+jennysueee@users.noreply.github.com>
2020-01-27 21:39:48 -0500

    Add files via upload

---
+++ sqlcode.sql
@@ -0,0 +1,10 @@
+create table netflix_hulu_catalog (
+  Show_ID VARCHAR(30) NOT NULL,
+  Show_Name VARCHAR(30) NOT NULL,
+  Genre VARCHAR(30) NOT NULL,
+  TV_Rating VARCHAR(30),
+  Description VARCHAR(30),
+  Available_on_Hulu VARCHAR(30),
+  Available_on_Netflix VARCHAR(30)
+	
+);
